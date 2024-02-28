e2a93c8c90c8e3621bd675e454531756028931b6
IsabelCollado <icolladoherrera@gmail.com>
2024-02-28 20:48:45 +0100

    Add packagejson react-latest-ui/react-sticky-notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 // Fichero src/component/App.js
 import '../styles/App.scss';
+import ReactStickyNotes from '@react-latest-ui/react-sticky-notes';
 
 const App = () => {
   return (
-    <div>
-      <h1>React+Vite</h1>
+    <div className="app">
+      <ReactStickyNotes />
     </div>
   );
 };
